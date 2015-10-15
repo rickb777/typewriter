9c4d40218069335bfe5e167ff3569eb320ce3d2d
Rick <rickb777@github.com>
2015-10-15 22:55:37 +0100

    Improved the documentation. Fixed the semantic error in MaxByT and MinByT, returning the wrong type. More tests.

--- type.go
+++ type.go
@@ -17,18 +17,23 @@ type Type struct {
 	types.Type
 }
 
-type test bool
-
-// a convenience for using bool in file name, see WriteAll
-func (t test) String() string {
-	if t {
-		return "_test"
-	}
-	return ""
-}
 
 func (t Type) String() (result string) {
 	return fmt.Sprintf("%s%s", t.Pointer.String(), t.Name)
+}
+
+func (t Type) Ptr() (result string) {
+	return t.Pointer.String()
+}
+
+// ValueOf inverts the star symbol, supplying * where a pointer value needs to be dereferenced.
+func (t Type) ValueOf() (result string) {
+	return t.Pointer.ValueOf()
+}
+
+// AddrOf returns & when this t a pointer type, allowing a template to take the address of a plain value.
+func (t Type) AddrOf() string {
+	return t.Pointer.AddrOf()
 }
 
 // LongName provides a name that may be useful for generated names.
@@ -57,12 +62,40 @@ func (t Type) FindTag(name string) (Tag, bool) {
 	return Tag{}, false
 }
 
+
 // Pointer exists as a type to allow simple use as bool or as String, which returns *
 type Pointer bool
 
 func (p Pointer) String() string {
 	if p {
 		return "*"
+	}
+	return ""
+}
+
+// ValueOf inverts the star symbol, supplying * where a pointer value needs to be dereferenced.
+func (p Pointer) ValueOf() string {
+	if p {
+		return ""
+	}
+	return "*"
+}
+
+// AddrOf returns & when this is a pointer, allowing a template to take the address of a plain value.
+func (p Pointer) AddrOf() string {
+	if p {
+		return "&"
+	}
+	return ""
+}
+
+
+type test bool
+
+// a convenience for using bool in file name, see WriteAll
+func (t test) String() string {
+	if t {
+		return "_test"
 	}
 	return ""
 }
